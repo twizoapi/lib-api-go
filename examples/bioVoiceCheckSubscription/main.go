84dbf9df0aaec3b03c2d5d85cbc3acb6ec39b32f
Michiel Brandenburg <apex@xepa.nl>
2018-07-11 11:44:49 +0200

    - Finished missing code for CheckSubscription and CheckRegistration, added examples - Added biovoice delete example - Added check for already existing biovoice registration

--- examples/bioVoiceCheckSubscription/main.go
+++ examples/bioVoiceCheckSubscription/main.go
@@ -17,15 +17,15 @@ func main() {
 	//
 	// Note: error handling was abbreviated for example's sake
 	//
-
 	//
 	// Biovoice might not work with a test key
 	//
 	utils.CheckKey(true)
-	registration, _ := utils.AskForInput("Create biovoice registration for phone number [6100000000]: ", "6100000000")
-	bioVoiceResponse, err := twizo.BioVoiceCreateRegistration(registration)
-	if aErr, ok := err.(*twizo.APIError); ok && aErr.Conflict() {
-		fmt.Printf("Biovoice for [%s] already created.\n", registration)
+
+	registration, _ := utils.AskForInput("Check biovoice subscription for phone number [6100000000]: ", "6100000000")
+	bioVoiceResponse, err := twizo.BioVoiceCheckSubscription(registration)
+	if aErr, ok := err.(*twizo.APIError); ok && aErr.NotFound() {
+		fmt.Printf("Biovoice subscription for [%s] not found.\n", registration)
 		os.Exit(1)
 	}
 	if err != nil {
--- examples/bioVoiceDelete/main.go
+++ examples/bioVoiceDelete/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	twizo "github.com/twizoapi/lib-api-go"
 	"github.com/twizoapi/lib-api-go/examples"
@@ -17,20 +16,16 @@ func main() {
 	//
 	// Note: error handling was abbreviated for example's sake
 	//
-
 	//
 	// Biovoice might not work with a test key
 	//
 	utils.CheckKey(true)
-	registration, _ := utils.AskForInput("Create biovoice registration for phone number [6100000000]: ", "6100000000")
-	bioVoiceResponse, err := twizo.BioVoiceCreateRegistration(registration)
-	if aErr, ok := err.(*twizo.APIError); ok && aErr.Conflict() {
-		fmt.Printf("Biovoice for [%s] already created.\n", registration)
-		os.Exit(1)
-	}
+
+	registration, _ := utils.AskForInput("Delete biovoice registration for phone number [6100000000]: ", "6100000000")
+	err := twizo.BioVoiceDeleteSubscription(registration)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%#v\n", bioVoiceResponse)
+	fmt.Printf("Deleted biovoice registration for [%s]\n", registration)
 }
